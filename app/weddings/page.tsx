73ede6cba3b2d9aa164565305fcb5ebfbc4cd707
keanu porral <kaype.dev@gmail.com>
2026-05-02 21:16:05 -0700

    weddings page fix

---
+++ app/weddings/page.tsx
@@ -0,0 +1,12 @@
+import FullWidthGallery from "@/components/work/FullWidthGallery"
+
+export default function Weddings() {
+    return (
+        <div>
+            <div className="w-full max-w-[1440px] mx-auto">
+                <FullWidthGallery/>
+            </div>
+           
+        </div>
+    )
+}
