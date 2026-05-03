ac37bad169cfb1a3a2d2822d87bcdd6eadbc1dc3
keanu porral <kaype.dev@gmail.com>
2026-05-02 21:24:32 -0700

    deploy fix 1

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,6 +1,6 @@
 import FullWidthGallery from "@/components/work/FullWidthGallery"
 
-export default function About() {
+export default function Work() {
     return (
         <div>
             <div className="w-full max-w-[1440px] mx-auto">
